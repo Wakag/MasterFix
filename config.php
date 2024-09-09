5b0757c727f49cdd7631a345b11bea2947673050
Makomborero Murwira <g22m3237@campus.ru.ac.za>
2024-09-09 16:27:06 +0200

    CHANGES TO CONFIG

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define("SERVER", "is3-dev.ict.ru.ac.za");
 define("USERNAME", "TheMasters");
 define("PASSWORD","Q8vrS8M8");
 define("DATABASE", "themasters");
-$mysqli = new mysqli($host, $username, $password, $database);
+$mysqli = new mysqli(SERVER,USERNAME,PASSWORD,DATABASE);
 
 // Check connection
 if ($mysqli->connect_error) {
