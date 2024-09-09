1580ca8540232edc2d36d8e404edd5b4ab22302d
Makomborero Murwira <g22m3237@campus.ru.ac.za>
2024-09-09 16:23:46 +0200

    edited config.php

--- config.php
+++ config.php
@@ -4,8 +4,10 @@ define("SERVER", "is3-dev.ict.ru.ac.za");
 define("USERNAME", "TheMasters");
 define("PASSWORD","Q8vrS8M8");
 define("DATABASE", "themasters");
-$conn = new mysqli(SERVER, USERNAME, PASSWORD, DATABASE);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error );
+$mysqli = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
 }
 ?>
